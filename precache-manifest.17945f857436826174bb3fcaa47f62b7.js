265a6065d891ea4ca8bbf42917994b8088491939
jykumarm <jykumarm@gmail.com>
2022-03-04 15:38:40 +0000

    deploy the bundle

---
+++ precache-manifest.17945f857436826174bb3fcaa47f62b7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d297cba845ec1b599d24a07362443d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7799c471054fc491d8",
+    "url": "/static/css/main.8b6a3131.chunk.css"
+  },
+  {
+    "revision": "aebc68f32c499fcede96",
+    "url": "/static/js/2.f40e1d35.chunk.js"
+  },
+  {
+    "revision": "1f856904e7e7ca93e4849cc435183cdc",
+    "url": "/static/js/2.f40e1d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7799c471054fc491d8",
+    "url": "/static/js/main.7eb38cfe.chunk.js"
+  },
+  {
+    "revision": "25adb8eba63c9e197782",
+    "url": "/static/js/runtime-main.c46e58d3.js"
+  },
+  {
+    "revision": "b6f1cde70e69ee0c5ff574567fbb56bb",
+    "url": "/static/media/About.b6f1cde7.svg"
+  },
+  {
+    "revision": "02b199b38bfe86185fb2c5d31602f02b",
+    "url": "/static/media/app.02b199b3.svg"
+  },
+  {
+    "revision": "773aa9391e26f6344402b93d48c02b59",
+    "url": "/static/media/arrow.773aa939.svg"
+  },
+  {
+    "revision": "bfac0a3a76f0bb243e2fe4174b2a06c4",
+    "url": "/static/media/bulb1.bfac0a3a.svg"
+  },
+  {
+    "revision": "6058040a5d6d9d6c79c21adfa5660ee9",
+    "url": "/static/media/close.6058040a.svg"
+  },
+  {
+    "revision": "ceb759ecaedd0e4e70490caff02bfa37",
+    "url": "/static/media/contact.ceb759ec.svg"
+  },
+  {
+    "revision": "27838e0b6ea57fbe0b235ff059a0ef25",
+    "url": "/static/media/ekg.27838e0b.svg"
+  },
+  {
+    "revision": "4b30194a725444be6283ebb14a978fd8",
+    "url": "/static/media/github.4b30194a.svg"
+  },
+  {
+    "revision": "fc3ed06095dee932ba9e02d6bea88e06",
+    "url": "/static/media/gmail.fc3ed060.svg"
+  },
+  {
+    "revision": "8b41ca2e4953bb7e8cc248acee31994a",
+    "url": "/static/media/home.8b41ca2e.svg"
+  },
+  {
+    "revision": "1fcf633ea92ac51a3a97ef1f4ef43f46",
+    "url": "/static/media/idea.1fcf633e.svg"
+  },
+  {
+    "revision": "218c39412a17cc99f2c4e7cc5f36354c",
+    "url": "/static/media/india.218c3941.svg"
+  },
+  {
+    "revision": "21a464a975f56c431e99cfb5767803f0",
+    "url": "/static/media/linkedin.21a464a9.svg"
+  },
+  {
+    "revision": "ed1967b7d4328bd339cb61b91638b385",
+    "url": "/static/media/p1-c250.ed1967b7.png"
+  },
+  {
+    "revision": "6f39098ec193f74febb3408825257214",
+    "url": "/static/media/p2-c250.6f39098e.png"
+  },
+  {
+    "revision": "84548eb74404c6deef3be2ee57a77cc5",
+    "url": "/static/media/phone.84548eb7.svg"
+  },
+  {
+    "revision": "929bb3f7e48d8fcdee27a35f2d61f491",
+    "url": "/static/media/ui.929bb3f7.svg"
+  }
+]);
